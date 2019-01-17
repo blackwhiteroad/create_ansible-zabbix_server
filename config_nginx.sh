d3345eb19e57a2068cc2d7d77e38b97811a18178
blackwhiteroad <1107906657@qq.com>
2019-01-17 15:10:33 +0800

    add yum -y install php-fpm

--- config_nginx.sh
+++ config_nginx.sh
@@ -2,7 +2,7 @@
 #脚本一键安装nginx
 yum clean all
 yum repolist
-yum -y install gcc pcre-devel openssl-devel
+yum -y install gcc pcre-devel openssl-devel php-fpm
 tar -zxf nginx-1.12.2.tar.gz
 cd nginx-1.12.2/
 ./configure --with-http_ssl_module
