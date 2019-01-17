298301a63bbe98f7549a939a14b6d4b454f6c292
blackwhiteroad <1107906657@qq.com>
2019-01-17 09:09:16 +0800

    configure nginx

---
+++ config_nginx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#脚本一键安装nginx
+yum clean all
+yum repolist
+yum -y install gcc pcre-devel openssl-devel
+tar -zxf nginx-1.12.2.tar.gz
+cd nginx-1.12.2/
+./configure --with-http_ssl_module
+make && make install
