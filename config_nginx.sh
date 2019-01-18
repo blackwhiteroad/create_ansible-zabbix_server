cead760e5305249951f6903866230d5f574edda7
blackwhiteroad <1107906657@qq.com>
2019-01-18 14:44:20 +0800

    add:1.yi(yum -y install) zlib-devel;2.Specifies the nginx new software directory;3.View nginx port information.

--- config_nginx.sh
+++ config_nginx.sh
@@ -2,8 +2,9 @@
 #脚本一键安装nginx
 yum clean all
 yum repolist
-yum -y install gcc pcre-devel openssl-devel php-fpm
+yum -y install gcc pcre-devel openssl-devel php-fpm zlib-devel
 tar -zxf nginx-1.12.2.tar.gz
 cd nginx-1.12.2/
-./configure --with-http_ssl_module
+./configure  --prefix=/usr/local/nginx --with-http_ssl_module
 make && make install
+netstat -anpltu | grep nginx
