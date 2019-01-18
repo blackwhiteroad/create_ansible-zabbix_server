27b01996ce3cdfb0d47d5a9b70f819d8855ced2f
blackwhiteroad <1107906657@qq.com>
2019-01-18 14:48:06 +0800

    add:1.yi(yum -y install) php-gd php-xml php-ldap;2.View php-fpm port information.

--- start_php-fpm_nginx_service.sh
+++ start_php-fpm_nginx_service.sh
@@ -4,9 +4,10 @@
 ln -s /usr/local/nginx/sbin/nginx /sbin/nginx
 nginx
 #2.启动php-fpm服务
-yum -y install php php-mysql
+yum -y install php php-mysql php-gd php-xml php-ldap
 systemctl start php-fpm
 systemctl enbale php-fpm
 start_php-fpm_nginx_service.sh
+netstat -antupl | grep php-fpm
 chmod +x start_php-fpm_nginx_service.sh
 start_php-fpm_nginx_service.sh
